67ffd6730d763ab91a819b0c8a7d5b683f5c7716
alageev <alageev5610@gmail.com>
2021-01-25 13:50:14 +0200

    Add actors

--- src/components/elements/Actor.js
+++ src/components/elements/Actor.js
@@ -1,5 +1,22 @@
-const Actor = () => {
-  return <div>Actor</div>;
+import { IMAGE_BASE_URL, POSTER_SIZE } from '../../config';
+import NoImage from '../images/no_image.jpg';
+import { StyledActor } from '../styles/StyledActor';
+
+const Actor = ({ actor }) => {
+  return (
+    <StyledActor>
+      <img
+        src={
+          actor.profile_path
+            ? `${IMAGE_BASE_URL}${POSTER_SIZE}${actor.profile_path}`
+            : NoImage
+        }
+        alt='Actor thumb'
+      />
+      <span className='actor-name'>{actor.name}</span>
+      <span className='actor-character'>{actor.character}</span>
+    </StyledActor>
+  );
 };
 
 export default Actor;
